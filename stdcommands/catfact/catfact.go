ee5d57b3e973f48e37f3843464df45e4db0c45a7
savage4618 <savage4618@users.noreply.github.com>
2022-10-27 18:37:42 -0400

    converted catfact command to use API instead of local db

--- stdcommands/catfact/catfact.go
+++ stdcommands/catfact/catfact.go
@@ -1,7 +1,10 @@
 package catfact
 
 import (
+	"encoding/json"
+	"io"
 	"math/rand"
+	"net/http"
 
 	"github.com/botlabs-gg/yagpdb/v2/commands"
 	"github.com/botlabs-gg/yagpdb/v2/lib/dcmd"
@@ -16,7 +19,53 @@ var Command = &commands.YAGCommand{
 	SlashCommandEnabled: true,
 
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		cf := Catfacts[rand.Intn(len(Catfacts))]
+		var cf string
+		var err error
+		request := rand.Intn(2)
+		if request > 0 {
+			cf, err = catFactFromAPI()
+			if err == nil {
+				return cf, nil
+			}
+		}
+		cf = Catfacts[rand.Intn(len(Catfacts))]
 		return cf, nil
 	},
+}
+
+func catFactFromAPI() (string, error) {
+	var cf struct {
+		Fact string `json:"fact"`
+	}
+
+	query := "https://catfact.ninja/fact"
+	req, err := http.NewRequest("GET", query, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("User-Agent", "RandomDadGeneralPurposeDiscordBot")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != 200 {
+		return "", commands.NewPublicError("Not 200!")
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	queryErr := json.Unmarshal([]byte(body), &cf)
+	if queryErr != nil {
+		return "", err
+	}
+
+	return cf.Fact, nil
 }
