ff2bc454fbfa962f2a17465d80163a0a5b568a4e
savage4618 <savage4618@users.noreply.github.com>
2022-11-15 18:36:45 -0500

    don't program with half your eyes. read everything with both eyes all the time.

--- stdcommands/cbbscore/cbbscore.go
+++ stdcommands/cbbscore/cbbscore.go
@@ -1,106 +1,3 @@
-// package cbbscore
-
-// import (
-// 	"fmt"
-// 	"io"
-// 	"net/http"
-// 	"net/url"
-
-// 	"github.com/botlabs-gg/yagpdb/v2/commands"
-// 	"github.com/botlabs-gg/yagpdb/v2/lib/dcmd"
-// )
-
-// // type NextEvent struct {
-// // 	Name         string         `json:"name"`
-// // 	Competitions []Competitions `json:"competitions"`
-// // }
-
-// // type Competitions struct {
-// // 	Notes      []Notes      `json:"notes"`
-// // 	Broadcasts []Broadcasts `json:"broadcasts"`
-// // 	Status     Status       `json:"status"`
-// // }
-
-// // type Notes struct {
-// // 	Headline string `json:"headline"`
-// // }
-
-// // type Broadcasts struct {
-// // 	Media Media `json:"media"`
-// // }
-
-// // type Media struct {
-// // 	ShortName string `json:"shortName"`
-// // }
-
-// // type Status struct {
-// // 	Clock  float64 `json:"clock"`
-// // 	Period int     `json:"period"`
-// // 	Type   Type    `json:"type"`
-// // }
-
-// // type Type struct {
-// // 	Description string `json:"description"`
-// // 	ShortDetail string `json:"shortDetail"`
-// // }
-
-// type Output struct {
-// 	Team struct {
-// 		NextEvent []struct {
-// 			Name         string `json:"name"`
-// 			Competitions []struct {
-// 				Broadcasts []struct {
-// 					Media struct {
-// 						ShortName string `json:"shortName"`
-// 					} `json:"media"`
-// 				} `json:"broadcasts"`
-// 				Status struct {
-// 					Type struct {
-// 						ShortDetail string `json:"shortDetail"`
-// 					} `json:"type"`
-// 				} `json:"status"`
-// 			} `json:"competitions"`
-// 		} `json:"nextEvent"`
-// 	} `json:"team"`
-// }
-
-// var Command = &commands.YAGCommand{
-// 	Cooldown:    5,
-// 	CmdCategory: commands.CategoryFun,
-// 	Name:        "cbbscore",
-// 	Description: "Grabs College Basketball score from ESPN API",
-// 	Arguments: []*dcmd.ArgDef{
-// 		{Name: "Team", Type: dcmd.String},
-// 	},
-// 	DefaultEnabled:      true,
-// 	SlashCommandEnabled: true,
-// 	RequiredArgs:        1,
-
-// 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-
-// 		addr := "https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/" + url.QueryEscape(data.Args[0].Str())
-
-// 		resp, err := http.Get(addr)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		defer resp.Body.Close()
-// 		body, err := io.ReadAll(resp.Body)
-// 		if err != nil {
-// 			return
-// 		}
-// 		output := &Output{}
-// 		err = json.Unmarshall([]byte(body), &output)
-// 		if err != nil {
-// 			return
-// 		}
-// 		msg := fmt.Printf("%+v\n", output)
-// 		fmt.Printf("%s\n", output.Team.NextEvent[0].Name)
-// 		fmt.Printf("%s\n", output.Team.NextEvent[0].Competitions[0].Broadcasts[0].Media.ShortName)
-// 		fmt.Printf("%s\n", output.Team.NextEvent[0].Competitions[0].Status.Type.ShortDetail)
-// 	},
-// }
-
 package cbbscore
 
 import (
@@ -132,8 +29,8 @@ var Command = &commands.YAGCommand{
 		if err != nil {
 			return nil, err
 		}
-
-		addrScore := "http://site/api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard/" + output.Team.NextEvent[0].Id
+		eventID := output.Team.NextEvent[0].Id
+		addrScore := "http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard/" + eventID
 
 		score, err := apiScoreSearch(addrScore)
 		if err != nil {
