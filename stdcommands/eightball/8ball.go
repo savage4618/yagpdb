f3c10a78037ebe389a3a7b5a6512f937478f650f
savage4618 <savage4618@users.noreply.github.com>
2023-07-07 10:46:52 -0400

    refine 8ball command

--- stdcommands/eightball/8ball.go
+++ stdcommands/eightball/8ball.go
@@ -1,50 +1,77 @@
+// using code from https://github.com/xbilldozer/8ball
+
 package eightball
 
 import (
 	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/botlabs-gg/yagpdb/v2/commands"
 	"github.com/botlabs-gg/yagpdb/v2/lib/dcmd"
 )
 
+var magicAnswers = [...]string{
+	// Standard set of Magic 8Ball answers.
+	// See https://en.wikipedia.org/wiki/Magic_8-Ball#Possible_answers
+
+	// positive outcomes
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt",
+	"Yes, definitely",
+	"You may rely on it",
+	"As I see it, yes",
+	"Most likely",
+	"Outlook good",
+	"Yes",
+	"Signs point to yes",
+
+	// Neutral outcomes
+	"Reply hazy try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+	// extra neutral outcomes
+	"That depends on what that means",
+	"wtf are you talking about?",
+	"that's what she said",
+
+	//Negative outcomes
+	"Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful",
+}
+
+var ShakeFailureMessage = "You can't just shake me bro, ask a question!"
+
 var Command = &commands.YAGCommand{
-	Cooldown:    5,
-	CmdCategory: commands.CategoryFun,
-	Name:        "8ball",
-	Description: "Ask the magic 8ball a question",
+	CmdCategory:         commands.CategoryFun,
+	Name:                "8ball",
+	Description:         "Ask the magic 8ball a question",
+	DefaultEnabled:      true,
+	SlashCommandEnabled: true,
 	Arguments: []*dcmd.ArgDef{
 		{Name: "Question", Type: dcmd.String},
 	},
-	DefaultEnabled:      true,
-	SlashCommandEnabled: true,
-	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		// Standard set of Magic 8Ball answers.
-		// See https://en.wikipedia.org/wiki/Magic_8-Ball#Possible_answers
-		answers := []string{
-			"It is certain",
-			"It is decidedly so",
-			"Without a doubt",
-			"Yes, definitely",
-			"You may rely on it",
-			"As I see it, yes",
-			"Most likely",
-			"Outlook good",
-			"Yes",
-			"Signs point to yes",
-			"Reply hazy try again",
-			"Ask again later",
-			"Better not tell you now",
-			"Cannot predict now",
-			"Concentrate and ask again",
-			"Don't count on it",
-			"My reply is no",
-			"My sources say no",
-			"Outlook not so good",
-			"Very doubtful",
-		}
 
-		q := data.Args[0].Str()
-		return fmt.Sprintf("> %s\n:8ball: %s", q, answers[rand.Intn(len(answers))]), nil
+	RunFunc: func(data *dcmd.Data) (interface{}, error) {
+		if data.SlashCommandTriggerData != nil {
+			return "You asked: " + data.Args[0].Str() + "\n" + Shake(data), nil
+		}
+		return Shake(data), nil
 	},
+}
+
+func Shake(data *dcmd.Data) string {
+	if data.Args[0].Str() == "" {
+		return ShakeFailureMessage
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	return fmt.Sprintf("8-Ball says: %s",
+		magicAnswers[rand.Intn(len(magicAnswers))])
 }
